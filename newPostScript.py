99f01751a768c7a549595c35ab5632bcaaec890b
xl-spec <augurship@gmail.com>
2024-04-22 17:59:10 -0400

    have more pictures, next step is to have one proper entry blog. also need to fix up the musicparser, lots of python next

--- newPostScript.py
+++ newPostScript.py
@@ -9,13 +9,14 @@ def add_post(new_post, filepath='src/data/posts.json'):
         file.truncate()
 
 new_post = {
-    "id": 3,
-    "artist": "Sobs",
+    "id": 6,
+    "artist": "Sodddbs",
     "name": "LOML",
     "myDate": "2024-04-18",
     "releaseDate": "2024-04-18",
     "genre": ["rock"],
-    "markdownPath": "posts/post3.md"
+    "markdownPath": f"https://raw.githubusercontent.com/xl-spec/Music_Blog/main/src/data/posts/tempblog.md",
+    "imageUrl": f"https://raw.githubusercontent.com/xl-spec/Music_Blog/main/MusicParser/Images/WjQKF1eD2Zs&si=m_1PaOlmVZWqbaa_.jpg"
 }
 
 add_post(new_post)
