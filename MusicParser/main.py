b6d60a2ba4776cfb369419594e580795d23d5cc2
xl-spec <augurship@gmail.com>
2024-05-02 17:39:25 -0400

    musicparser prob almost completely finished, need a way to edit it elegantly but that's it

--- MusicParser/main.py
+++ MusicParser/main.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import json
 from bs4 import BeautifulSoup
 from colorthief import ColorThief
 from PIL import Image
@@ -7,7 +8,6 @@ import re
 class MusicParser():
     def __init__(self):
         self.id = None
-        self.color_wheel = []
         self.palette = []
         self.data = {
             'title': None,
@@ -43,11 +43,10 @@ class MusicParser():
         print(self.data)
         return self.data
 
-    def process_description(self, description_content):
-        # function to get the song name & artists
+    def process_description(self, description_content): # function to get the song name & artists
         lines = description_content.split('\n')
         infoFound = False
-        print(lines)
+
         for line in lines:
             if '·' in line:
                 parts = line.split('·')
@@ -57,9 +56,7 @@ class MusicParser():
                     self.data['artist'] = [part.strip() for part in parts[1:]]
                     break
         
-        if not infoFound:
-            # temp fix for mashup/edited songs. 
-            # These songs don't have a autoyoutube generated description, i will edit manually
+        if not infoFound: # If songs don't have a autoyoutube generated description, i will edit manually
             self.data['title'] = ''
             self.data['artist'] = ''
 
@@ -67,8 +64,7 @@ class MusicParser():
         if date_match:
             self.data['release_date'] = date_match.group(1)
             
-    def getArt(self):
-        # puts picture into folder
+    def getArt(self): # puts picture into folder
         html_url = f"https://www.youtube.com/watch?v={self.id}"
         response = requests.get(html_url)
         soup = BeautifulSoup(response.content, "html.parser")
@@ -84,39 +80,35 @@ class MusicParser():
         self.makeSquare(os.path.join(album_art_dir, f"{self.fileName}.jpg"))
         print(f"Picture made")
     
-    def getColorWheel(self):
-        # color wheel, will expand this later and pass this through the css
-        color_thief = ColorThief(f"../src/data/album_arts/{self.fileName.id}.jpg")
+    def getColorWheel(self): # color wheel, will expand this later and pass this through the css
+        color_thief = ColorThief(f"../src/data/album_arts/{self.fileName}.jpg")
         palette = color_thief.get_palette(color_count=3, quality=1)
-        self.color_wheel = palette
+        self.palette = palette
     
-    def printColorWheel(self):
-        print(self.color_wheel)
-        for count, color in enumerate(self.color_wheel):
+    def printColorWheel(self): # idk i might delete
+        print(self.palette)
+        for count, color in enumerate(self.palette):
             print(f'\033[38;2;{color[0]};{color[1]};{color[2]}mColor{count + 1}\033[0m', end=" ")
     
-    def makeSquare(self, img_path):
-        # crops the image to be a perfect square
+    def makeSquare(self, img_path): # crops the image to be a perfect square
         with Image.open(img_path) as img:
-            # print(f"Original dimensions: {img.size}")
             min_dim = min(img.size)
             left = (img.width - min_dim) // 2
             top = (img.height - min_dim) // 2
             right = (img.width + min_dim) // 2
             bottom = (img.height + min_dim) // 2
-            # crop it so it's a square in the middle
             img_cropped = img.crop((left, top, right, bottom))
             img_cropped.save(img_path)
             print(f"Cropped to square dimensions: {img_cropped.size}")
     
-    def setFileName(self):
+    def setFileName(self): # sets the filename for jpg + md
         # the -- is to represent multiple artists
         artist = "--".join(artist.replace(" ", "-") for artist in self.data['artist'])
         title = self.data['title'].replace(" ", "-")
         self.fileName = f"{artist}_{title}".lower()
         print(f"File Set: {self.fileName}")
 
-    def createPost(self): # makes the md file
+    def createMd(self): # makes the md file
         post_dir = os.path.join(os.path.dirname(__file__), "../src/data/posts")
         post_file_path = os.path.join(post_dir, f"{self.fileName}.md")
         with open(post_file_path, "w") as f:
@@ -124,15 +116,59 @@ class MusicParser():
 
         print(f"Post created: {post_file_path}")
 
+    def genPost(self):
+        post_dir = os.path.join(os.path.dirname(__file__), "../src/data")
+        # post_file_path = os.path.join(post_dir, f"{self.fileName}.md")
+        new_post = {
+            "id": self.id,
+            "artist": self.data['artist'],
+            "name": self.data['title'],
+            "myDate": 'todayyyy',
+            "releaseDate": self.data['release_date'],
+            "type": self.type,
+            "palette": self.palette,
+            "genre": ["To be added"],
+            "markdownPath": f"https://raw.githubusercontent.com/yourusername/Music_Blog/main/src/data/posts/{self.fileName}.md",
+            "imageUrl": f"https://raw.githubusercontent.com/yourusername/Music_Blog/main/src/data/album_arts/{self.fileName}.jpg"
+        }
 
+        # for i in new_post:
+        #     print(new_post[i])
+
+        new_post_path = os.path.join(post_dir, "newPost.json")
+        with open(new_post_path, 'w') as file:
+            json.dump(new_post, file, indent=2)
+        print("New post data saved:", new_post_path)
+
+    def addPost(self):
+        # This function reads newPost.json and adds its content to posts.json
+        post_dir = os.path.join(os.path.dirname(__file__), "../src/data")
+        new_post_path = os.path.join(post_dir, "newPost.json")
+        posts_json_path = os.path.join(post_dir, "posts.json")
+
+        # Load the new post from newPost.json
+        with open(new_post_path, 'r') as file:
+            new_post = json.load(file)
+
+        # Load the existing posts and append the new post
+        with open(posts_json_path, 'r+') as file:
+            posts = json.load(file)
+            posts['posts'].append(new_post)
+            file.seek(0)
+            json.dump(posts, file, indent=2)
+            file.truncate()
+        print("New post added to posts.json")
+        
 parser = MusicParser()
-link = input("enter link:\n")
-parser.setId(link)
-# parser.setId("https://music.youtube.com/watch?v=-BLNfBB6D2Y&si=Lvl2XJytJT7LO_bB")
+# link = input("enter link:\n")
+# parser.setId(link)
+parser.setId("https://music.youtube.com/watch?v=yCkLC9vQ_Fg&si=v3lC1rKrHiDyS5iq")
 parser.getData()
 parser.setFileName()
 parser.getArt()
-parser.createPost()
+parser.createMd()
+parser.getColorWheel()
+parser.genPost()
+parser.addPost()
 
-# parser.getColorWheel()
 # parser.printColorWheel()
