7bda5cea0c61f65abc8bcf7e4def972dae556ae9
xl-spec <augurship@gmail.com>
2024-05-13 19:04:54 -0400

    whoops wrong json

--- MusicParser/main.py
+++ MusicParser/main.py
@@ -215,7 +215,8 @@ class GUI(tk.Tk):
             "palette": self.parser.palette,
             "genre": ["To be added"],
             "title": self.parser.title,
-            "image": f"../src/data/album_arts/{self.parser.fileName}.jpg"  # Local path for Tkinter display
+            "markdownPath": f"https://raw.githubusercontent.com/xl-spec/Music_Blog/main/src/data/posts/{self.fileName}.md",
+            "imageUrl": f"https://raw.githubusercontent.com/xl-spec/Music_Blog/main/src/data/album_arts/{self.fileName}.jpg"
         }
         self.json_text.delete(1.0, tk.END)
         self.json_text.insert(tk.END, json.dumps(json_data, indent=2))
