1c337a2b71c18db8cfaece1f3b58da9059698b88
xl-spec <augurship@gmail.com>
2024-05-16 15:30:53 -0400

    spotify link for blogpages

--- src/layout/HomeGalleryView.js
+++ src/layout/HomeGalleryView.js
@@ -9,7 +9,7 @@ const HomeGalleryView = ({ title, name, artist, imageUrl, linkUrl, palette }) =>
 
   return (
     <Link to={linkUrl} className="home-gallery-link">
-      <div className="home-gallery-post"  style={{ backgroundColor: paletteMapped[1] }} >
+      <div className="home-gallery-post" style={{ backgroundImage: `linear-gradient(to right, ${paletteMapped[0]}, ${paletteMapped[1]})` }}>
         <img src={imageUrl} alt={name} className="home-gallery-image" style={{ borderColor: paletteMapped[3] }}/>
         <div className="home-gallery-details" style={{ color: paletteMapped[0] }}>
           <h2 className="home-gallery-title">{title}</h2>
